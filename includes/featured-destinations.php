5f515fc266dd613faf681b361290f2aa23627020
marct <marc.takeuchi@gmail.com>
2017-12-29 19:40:17 -0800

    ^adding new featured destinations

--- includes/featured-destinations.php
+++ includes/featured-destinations.php
@@ -25,8 +25,40 @@
 					<a href="ewa-beach-airport-shuttle" title="Ewa Beach Airport Shuttle Rates"><img src="graphics/ewa-beach-map.jpg" alt="Ewa Beach Airport Shuttle Map"></a>
 					<h3><a href="ewa-beach-airport-shuttle" title="Ewa Beach Airport Shuttle Rates">Ewa Beach</a></h3>
 					<p>Airport Shuttle Services to and from Ewa Beach</p>
-					<a href="ewa-beach-airport-shuttle" class="btn btn-default" title="Ewa Beach Airport Shuttle Rates">Rates & Info</a>
+					<a href="ewa-beach-airport-shuttle" class="btn btn-default" title="Ewa Beach Airport Shuttle Rates">
+					Rates & Info</a>
+				</div>  
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<div class="cta-spot">
+					<a href="kailua-airport-shuttle" title="Kailua Airport Shuttle Rates"><img src="graphics/kailua-map.jpg" alt="Kailua Airport Shuttle Map"></a>
+					<h3><a href="waikiki-airport-shuttle" title="Waikiki Airport Shuttle Rates">Kailua</a></h3>
+					<p>Airport Shuttle Services to and from Kailua</p>
+					<a href="kailua-airport-shuttle" class="btn btn-default" title="Kailua Airport Shuttle Rates">Rates & Info</a>
 				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-4">
+				<div class="cta-spot">
+					<a href="kaneohe-airport-shuttle" title="Kaneohe Airport Shuttle Rates"><img src="graphics/kaneohe-map.jpg" alt="Hawaii Kai Airport Shuttle Map"></a>
+					<h3><a href="kaneohe-airport-shuttle" title="Kaneohe Airport Shuttle Rates">Kaneohe</a></h3>
+					<p>Airport Shuttle Services to and from Kaneohe</p>
+					<a href="kaneohe-airport-shuttle" class="btn btn-default" title="Kaneohe Airport Shuttle Rates">
+					Rates & Info</a>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-4">
+				<div class="cta-spot">
+					<a href="mililani-airport-shuttle" title="Mililani Airport Shuttle Rates"><img src="graphics/mililani-map.jpg" alt="Mililani Airport Shuttle Map"></a>
+					<h3><a href="mililani-airport-shuttle" title="Mililani Airport Shuttle Rates">Mililani</a></h3>
+					<p>Airport Shuttle Services to and from Mililani</p>
+					<a href="mililani-airport-shuttle" class="btn btn-default" title="Mililani Airport Shuttle Rates">
+					Rates & Info</a>
+				</div>  
 			</div>
 		</div>
 	</div>
