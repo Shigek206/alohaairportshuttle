cfd88c98085f4aee5e4019243d52ffc1c19e738e
marct <marc.takeuchi@gmail.com>
2018-01-29 20:15:56 -0800

    ^updating GA code

--- arrival-information.php
+++ arrival-information.php
@@ -17,15 +17,14 @@
 		<link href="styles/style.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="styles/featherlight.min.css" rel="stylesheet" type="text/css" media="all" />
 
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28002635-1']);
-		  _gaq.push(['_trackPageview']);
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-28002635-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-28002635-1');
 		</script>
 
 	</head>
