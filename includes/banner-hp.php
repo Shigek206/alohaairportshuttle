a75f937fd1e121729e1782dd114b0839e6b700bd
marct <marc.takeuchi@gmail.com>
2020-04-18 13:26:49 -0700

    ^separated out banner section to includes folder and added styles.

---
+++ includes/banner-hp.php
@@ -0,0 +1,14 @@
+<section class="banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>COVID-19 Update</h2>
+				<p>We at Aloha Airport Shuttle care about the health and safety of our customers and recommend people follow guidelines to remain safe and healthy. We went to notify travelers to Hawaii of the <a href="https://hidot.hawaii.gov/coronavirus/" target="_blank">14-day self quarantine rule</a> currently in effect and ask people to act responsibly.</p>
+
+				<p>We are open to serving our customers and are taking reservations for future trips. We look forward to Hawaii and the rest of the United States recovering and returning to normal. For updated travel information please visit the <a href="https://www.cdc.gov/coronavirus/2019-ncov/travelers/index.html" target="_blank">CDC Travel</a> page. Thank you for your understanding.</p>
+
+				<p>Mahalo, <strong>Aloha Airport Shuttle</strong></p>
+			</div>
+		</div>
+	</div>
+</section>
